48ddbec2b882a400577df88b4a0e7a5928e8c67b
theo05-dev <theo.hussne05@gmail.com>
2025-11-17 07:57:46 -0300

    Update ingest.py

--- app/ingest.py
+++ app/ingest.py
@@ -8,7 +8,6 @@ def initialize_rag(working_dir="db"):
     """
     return LightRAG(
         working_dir,
-        graph_enabled=False,
         enable_graph_index=False,
         graph_mode="none",
         enable_llm_for_edges=False,
